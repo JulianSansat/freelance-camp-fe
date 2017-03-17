76e83d0b80bdb68424ac308b63b66703f83ee8c1
JulianSansat <kerarchie@yahoo.com.ar>
2017-03-17 15:16:24 -0300

    front end modifications

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -4,30 +4,32 @@ import { Document } from './documents';
 @Component({
   moduleId: module.id,
   selector: 'documents',
-  templateUrl: 'documents.component.html'
+  templateUrl: 'documents.component.html',
+  styleUrls: ['documents.component.css']
 })
 export class DocumentsComponent {
+  pageTitle: string = "Document Dashboard"
   documents: Document[] = [
-    {
-      title: "My First Doc",
-      description: 'aadasdas asdasdasd',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'http://google.com',
-    },
-    {
-      title: "My Second Doc",
-      description: 'aadasdas asdasdasd',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'http://google.com',
-    },
-    {
-      title: "My Third Doc",
-      description: 'aadasdas asdasdasd',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'http://google.com',
-    }
+  {
+    title: "My First Doc",
+    description: 'asdfasdfasdf asdfasd',
+    file_url: 'http://google.com',
+    updated_at: '11/11/16',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
+  },
+  {
+    title: "My Second Doc",
+    description: 'asdfasdfasdf asdfasd',
+    file_url: 'http://google.com',
+    updated_at: '11/11/16',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
+  },
+  {
+    title: "My Last Doc",
+    description: 'asdfasdfasdf asdfasd',
+    file_url: 'http://google.com',
+    updated_at: '11/11/16',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
+  }
   ]
 }
